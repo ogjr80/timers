00a27f4d3656c86b35ecb75e7bced6e8cec616bf
Gideon Ogongo <ogjr80@gmail.com>
2017-10-09 00:05:09 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'; 
+import ReactDOM from 'react-dom'; 
+
+import TimersDashboard from './TimersDashboard'; 
+
+
+ReactDOM.render(
+    <TimersDashboard />, document.getElementById('root')
+)
